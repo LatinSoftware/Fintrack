d991a6a21778c9ef6e9a9180c5fa10473931f71f
Caonabo Mena <cmena7512@gmail.com>
2025-04-26 23:16:42 -0400

    Remove 'in' modifier from ICommandHandler interface definitions for consistency

--- Fintrack.ApiService/Shared/Abstractions/ICommand.cs
+++ Fintrack.ApiService/Shared/Abstractions/ICommand.cs
@@ -13,12 +13,12 @@ public interface ICommand<TResponse> : IRequest<Result<TResponse>> where TRespon
 
 }
 
-public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand, Result> where TCommand : ICommand
+public interface ICommandHandler<TCommand> : IRequestHandler<TCommand, Result> where TCommand : ICommand
 {
 
 }
 
-public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>> where TCommand : ICommand<TResponse> where TResponse : class
+public interface ICommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>> where TCommand : ICommand<TResponse> where TResponse : class
 {
 
 }
