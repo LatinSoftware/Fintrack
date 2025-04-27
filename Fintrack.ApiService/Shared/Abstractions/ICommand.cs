d8e5906b9087dc262530a16e3afb73a30fef09a0
Caonabo Mena <cmena7512@gmail.com>
2025-04-26 23:06:26 -0400

    Add ICommandHandler interfaces for command handling

--- Fintrack.ApiService/Shared/Abstractions/ICommand.cs
+++ Fintrack.ApiService/Shared/Abstractions/ICommand.cs
@@ -13,3 +13,13 @@ public interface ICommand<TResponse> : IRequest<Result<TResponse>> where TRespon
 
 }
 
+public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand, Result> where TCommand : ICommand
+{
+
+}
+
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>> where TCommand : ICommand<TResponse> where TResponse : class
+{
+
+}
+
