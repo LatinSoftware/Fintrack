cf2b73a59c10721f980e6d7247354787e32f2ede
Caonabo Mena <cmena7512@gmail.com>
2025-04-28 19:56:01 -0400

    add web

---
+++ Fintrack.Web/vite.config.js
@@ -0,0 +1,30 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { loadEnv } from 'vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      TanStackRouterVite({ autoCodeSplitting: true }),
+      viteReact(),
+      tailwindcss(),
+    ],
+    server: {
+      port: parseInt(env.VITE_PORT),
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
+  }
+})
